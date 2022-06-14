882b097d8093397f76eb42a3f56738b5d2a64911
yassine299 <yassinehajjoubi299@gmail.com>
2022-06-14 19:50:08 +0100

    add first version

---
+++ app.js
@@ -0,0 +1,29 @@
+const cardElemnt = document.querySelector("#cards");
+const element = document.getElementById("mybtn");
+const reset = document.getElementById("#reset");
+
+const Reset = () => {
+    window.location.reload();
+}
+
+const search = async () => {
+    const searchWord = document.querySelector("input").value;
+    const Url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=" + searchWord;
+    const response = await fetch(Url);
+    const json = await response.json();
+
+    json.data.forEach((card) => {
+        card.card_images.forEach((img) => {
+            const image = document.createElement("img");
+           // console.log(img.image_url)
+            image.src = img.image_url;
+            cardElemnt.append(image);
+
+        })
+    });
+
+}
+
+
+
+element.addEventListener("click", search);
